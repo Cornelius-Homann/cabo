40bf7d6dd8a853a37f62f78ba69805cfa0148121
Cornelius Homann <systemcraftmini@gmail.com>
2026-08-10 01:36:04 +0200

    Strip all build tools and revert to ultra-clean static HTML/CSS/JS for Vercel

--- Cabo-Game/public/cabo/firebase.js
+++ Cabo-Game/public/cabo/firebase.js
@@ -1,16 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
 
-const env = typeof import.meta !== 'undefined' && import.meta.env ? import.meta.env : {};
-
 const firebaseConfig = {
-  apiKey: env.VITE_FIREBASE_API_KEY || "AIzaSyD6d3XpQRiZS6ZX2d_8soi8XfUyCzGt4R4",
-  authDomain: env.VITE_FIREBASE_AUTH_DOMAIN || "cornelius-cabo.firebaseapp.com",
-  databaseURL: env.VITE_FIREBASE_DATABASE_URL || "https://cornelius-cabo-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: env.VITE_FIREBASE_PROJECT_ID || "cornelius-cabo",
-  storageBucket: env.VITE_FIREBASE_STORAGE_BUCKET || "cornelius-cabo.firebasestorage.app",
-  messagingSenderId: env.VITE_FIREBASE_MESSAGING_SENDER_ID || "817971372989",
-  appId: env.VITE_FIREBASE_APP_ID || "1:817971372989:web:3d94b5a5ff3ca225fce8d4"
+  apiKey: "AIzaSyD6d3XpQRiZS6ZX2d_8soi8XfUyCzGt4R4",
+  authDomain: "cornelius-cabo.firebaseapp.com",
+  databaseURL: "https://cornelius-cabo-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "cornelius-cabo",
+  storageBucket: "cornelius-cabo.firebasestorage.app",
+  messagingSenderId: "817971372989",
+  appId: "1:817971372989:web:3d94b5a5ff3ca225fce8d4"
 };
 
 // Initialize Firebase
